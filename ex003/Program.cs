e1b07bb5df405c9c5bf43591754424d02ea4fab9
MaksimS <sipatovGT@mail.ru>
2022-10-17 16:12:07 +0400

    ex003

--- ex003/Program.cs
+++ ex003/Program.cs
@@ -5,10 +5,15 @@ Console.WriteLine("Программа выдает дни недели по за
 Console.WriteLine("Введите число от 1 до 7: ");
 int number = int.Parse(Console.ReadLine()!);
 
-if(number == 1) Console.WriteLine("Monday");
-if(number == 2) Console.WriteLine("Tuesday");
-if(number == 3) Console.WriteLine("Wednesday");
-if(number == 4) Console.WriteLine("Thursday");
-if(number == 5) Console.WriteLine("Friday");
-if(number == 6) Console.WriteLine("Saturday");
-if(number == 7) Console.WriteLine("Sunday");
+if ((number < 8)&&(number > 0) ){
+    if(number == 1) Console.WriteLine("Monday");
+    if(number == 2) Console.WriteLine("Tuesday");
+    if(number == 3) Console.WriteLine("Wednesday");
+    if(number == 4) Console.WriteLine("Thursday");
+    if(number == 5) Console.WriteLine("Friday");
+    if(number == 6) Console.WriteLine("Saturday");
+    if(number == 7) Console.WriteLine("Sunday");
+}
+else {
+    Console.WriteLine("Произошла ошибка");
+    }
