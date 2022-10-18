22fa4fcb529ca44b95968771220d60039352b6c4
MaksimS <sipatovGT@mail.ru>
2022-10-18 17:36:53 +0400

    S2Z10

---
+++ S2Z10/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+// 1. Генерируем случайное трёхзначное число.
+int GenerateRandomNumber()
+{
+    return new Random().Next(100,1000); //трёхзначное число.
+}
+// 2. Удаляем вторую цифру этого числа.
+int FinalDigit(int randomNumber)
+{
+    Console.WriteLine(randomNumber);
+    int firstDigit = randomNumber / 100;
+    int secondDigit = (randomNumber - firstDigit*100) / 10;
+    return (secondDigit);
+}
+int randomNumber = GenerateRandomNumber();
+Console.WriteLine(FinalDigit(randomNumber));
