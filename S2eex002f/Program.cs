483dbef0d7c1239a624018ab8492bc762536a669
MaksimS <sipatovGT@mail.ru>
2022-10-18 12:48:47 +0400

    S2eex002f

---
+++ S2eex002f/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая вводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46 ; 782 -> 72 ; 918 -> 98.
+
+// 1. Генерируем случайное трёхзначное число.
+int GenerateRandomNumber()
+{
+    return new Random().Next(100,1000); //трёхзначное число.
+}
+// 2. Удаляем вторую цифру этого числа.
+int OutSecondDigit(int randomNumber)
+{
+    Console.WriteLine(randomNumber);
+    int firstDigit = randomNumber / 100;
+    int secondDigit = (randomNumber - firstDigit*100) / 10;
+    int thirdDigit = randomNumber % 10;
+    return (firstDigit*10 + thirdDigit);
+}
+int randomNumber = GenerateRandomNumber();
+Console.WriteLine(OutSecondDigit(randomNumber));
