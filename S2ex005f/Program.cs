27217d26d3c22dd80f8388bbac38bfa614942851
MaksimS <sipatovGT@mail.ru>
2022-10-18 15:44:28 +0400

    S2ex002f

---
+++ S2ex005f/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом второго.
+// 5-> 25; -4-> 16; 25-> 5; 8->9.
+
+Console.WriteLine("Программа Проверяет, является ли первое число, квадратом второго и наоборот");
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (IsSquare(a, b))
+{
+    Console.WriteLine("Да, кратно");
+}
+else
+{
+    Console.WriteLine("Нет, не кратно");
+}
+
+bool IsSquare(int a, int b)// функция
+{
+    return a == b * b || b == a * a;
+}
