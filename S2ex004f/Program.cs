9a6ee296d93e736418e021a71e88faac02bee72a
MaksimS <sipatovGT@mail.ru>
2022-10-18 15:00:08 +0400

    S2ex004f krat

---
+++ S2ex004f/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. (14, 46, 161).
+
+Console.WriteLine("Программа Проверяет, кратно ли Ваше число одновременно 7 и 23");
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (multiplicity(a))
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+  Console.WriteLine("Не кратно");  
+}
+
+bool multiplicity(int a)
+{
+    return a % 7 == 0 && a % 23 == 0;
+}
